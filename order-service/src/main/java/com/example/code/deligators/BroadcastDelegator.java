c3966fb9b2cff83f69f210355eb917c8be0e97ab
nt1dc <nt1dcare@gmail.com>
2023-06-24 13:25:05 +0300

    Feature: sending mails to user

--- order-service/src/main/java/com/example/code/deligators/BroadcastDelegator.java
+++ order-service/src/main/java/com/example/code/deligators/BroadcastDelegator.java
@@ -6,8 +6,16 @@ import com.example.code.repositories.UserRepository;
 import com.example.code.services.DeliveryService.DeliveryServiceLitRes;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailSendException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,9 +25,19 @@ public class BroadcastDelegator implements JavaDelegate {
     private final DeliveryServiceLitRes deliveryServiceLitRes;
     private final UserRepository userRepository;
 
-    public BroadcastDelegator(DeliveryServiceLitRes deliveryServiceLitRes, UserRepository userRepository) {
+    @Value("${mail.topic}")
+    private String topic;
+
+    @Value("${spring.mail.username}")
+    private String fromUser;
+
+    @Autowired
+    private final JavaMailSender javaMailSender;
+
+    public BroadcastDelegator(DeliveryServiceLitRes deliveryServiceLitRes, UserRepository userRepository, JavaMailSender javaMailSender) {
         this.deliveryServiceLitRes = deliveryServiceLitRes;
         this.userRepository = userRepository;
+        this.javaMailSender = javaMailSender;
     }
 
     @Override
@@ -31,11 +49,41 @@ public class BroadcastDelegator implements JavaDelegate {
             UserInfo userInfo = userRepository.findByUsername(orderDTO.getUser().getUsername()).orElseThrow();
             if (userInfo.isNotificated()) {
                 String email = userInfo.getEmail();
-                usersToMessages.put(email, orderDTO.getUser().getEmail());
+                usersToMessages.put(email, orderDTO.getOrderStatus().getValue());
 
             }
         });
 
-        usersToMessages.keySet().forEach(email -> System.out.println(usersToMessages.get(email)));
+        usersToMessages.keySet().forEach(email ->
+                send(email, usersToMessages.get(email)));
+
+    }
+
+
+
+
+    @Async
+    public void send(String to, String message) {
+        try {
+            MimeMessage mimeMessage = createMessage(to, message);
+            javaMailSender.send(mimeMessage);
+        } catch (MessagingException | MailSendException e) {
+            throw new IllegalStateException("Failed to send email to " + to);
+        }
+    }
+
+    private MimeMessage createMessage(String to, String message) throws MessagingException {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        setHelper(mimeMessage, to, message);
+        return mimeMessage;
+    }
+
+    private void setHelper(MimeMessage mimeMessage, String to, String message) throws MessagingException {
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "utf-8");
+
+        helper.setFrom(fromUser);
+        helper.setTo(to);
+        helper.setSubject(topic);
+        helper.setText(message, true);
     }
 }
